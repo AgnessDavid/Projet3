67fbc7f322e482552504a79eb32fa275e5ec8194
Agness David <119672669+AgnessDavid@users.noreply.github.com>
2025-08-27 12:56:48 +0000

    Workspace

---
+++ database/migrations/2025_08_27_105146_create_authors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->text('bio')->nullable();
+            $table->string('youtube_link')->nullable();  // lien YouTube
+            $table->string('photo_path')->nullable();    // pour stocker la photo
+            $table->string('file_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('authors');
+    }
+};
